b24ccfa45d7dc25a2c799a7ff72ed72183797535
Alex C-G <alexcg@outlook.com>
2021-06-23 12:06:57 +0200

    feat(image search): working, more config, get url directly

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
 import streamlit as st
 from tempfile import NamedTemporaryFile
-from config import text_endpoint, image_endpoint, images_path
+from config import text_endpoint, image_endpoint, images_path, top_k, image_size
 import requests
-from io import StringIO
-import base64
 
-image_size = 128
+
 
 def encode_to_base64(byte_string):
-    """
-    Encode image file to base64
-    """
-    output = str(base64.b64encode(byte_string), 'utf-8')
+    import base64
+
+    output = str(base64.b64encode(byte_string), "utf-8")
 
     return output
 
+
 def create_query(query: str, top_k: int, endpoint: str) -> list:
-    data = (
-        '{"top_k":' + str(top_k) + ', "mode": "search", "data":' + query + "}"
-    )
+    data = '{"top_k":' + str(top_k) + ', "mode": "search", "data":' + query + "}"
     response = requests.post(endpoint, headers=headers, data=data)
 
     content = response.json()["search"]["docs"]
@@ -30,25 +26,26 @@ def create_query(query: str, top_k: int, endpoint: str) -> list:
 
     return results
 
-def decode_base64_to_image(base64_string):
-    """
-    Decode base64 string back to image
-    """
-    pass
-
 
 def get_images(query: str, endpoint: str, top_k: int) -> dict:
     headers = {
         "Content-Type": "application/json",
     }
 
-    data = '{"parameters": {"top_k": ' + str(top_k) + '}, "mode": "search",  "data": ["data:image/png;base64,' + query + '"]}'
+    data = (
+        '{"parameters": {"top_k": '
+        + str(top_k)
+        + '}, "mode": "search",  "data": ["data:image/png;base64,'
+        + query
+        + '"]}'
+    )
 
     response = requests.post(endpoint, headers=headers, data=data)
     content = response.json()
     matches = content["data"]["docs"][0]["matches"]
 
     return matches
+
 
 def get_data(query: str, endpoint: str, top_k: int) -> dict:
     headers = {
@@ -63,6 +60,7 @@ def get_data(query: str, endpoint: str, top_k: int) -> dict:
     matches = content["data"]["docs"][0]["matches"]
 
     return matches
+
 
 # layout
 max_width = 1200
@@ -88,14 +86,46 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-st.title("Jina Meme Search")
+# Sidebar
+st.sidebar.title("Jina Meme Search")
 modality = st.sidebar.radio(
     label="I want to search using...", options=("Image", "Text")
 )
 
+settings = st.sidebar.beta_expander(label="Settings", expanded=False)
+with settings:
+    if modality == "Text":
+        endpoint = st.text_input(label="Endpoint", value=text_endpoint)
+    else:
+        endpoint = st.text_input(label="Endpoint", value=image_endpoint)
+
+    top_k = st.number_input(label="Top K", value=top_k, step=1)
+
+st.sidebar.markdown(
+    """
+### About
+
+This is an example meme search engine using the [Jina neural search framework](https://github.com/jina-ai/jina/).
+
+**Note: click the search button instead of hitting Enter. We're working on fixing this!**
+
+- Backend: [Jina](https://github.com/jina-ai/jina/)
+- Frontend: [Streamlit](https://www.streamlit.io/)
+
+[Visit the repo](https://github.com/alexcg1/jina-meme-search-example)
+
+---
+
+<a href="https://github.com/jina-ai/jina/"><img src="https://github.com/alexcg1/jina-app-store-example/blob/a8f64332c6a5b3ae42df07d4bd615ff1b7ece4d9/frontend/powered_by_jina.png?raw=true" width=256></a>
+""",
+    unsafe_allow_html=True,
+)
+
 if modality == "Text":
+    st.title("Search memes by caption")
     query = st.text_input(label="Search for a meme based on caption")
 else:
+    st.title("Search memes by similar image")
     query = st.file_uploader("Upload image")
     if query is not None:
         image_data = query.read()
@@ -112,31 +142,9 @@ if st.button(label="Search"):
         all_cells = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9]
 
         if modality == "Text":
-            matches = get_data(query=query, endpoint=text_endpoint, top_k=10)
+            matches = get_data(query=query, endpoint=endpoint, top_k=top_k)
         elif modality == "Image":
-            matches = get_images(query=query, endpoint=image_endpoint, top_k=10)
+            matches = get_images(query=query, endpoint=endpoint, top_k=top_k)
 
         for cell, match in zip(all_cells, matches):
-            if modality == "Text":
-                cell.image("http:" + match["tags"]["image_url"])
-            elif modality == "Image":
-                cell.image(match["uri"])
-
-
-# Sidebar
-st.sidebar.title("Jina Meme Search")
-st.sidebar.markdown(
-    """
-This is an example meme search engine using the [Jina neural search framework](https://github.com/jina-ai/jina/).
-
-**Note: click the search button instead of hitting Enter. We're working on fixing this!**
-
-- Backend: [Jina](https://github.com/jina-ai/jina/)
-- Frontend: [Streamlit](https://www.streamlit.io/)
-
-[Visit the repo](https://github.com/alexcg1/jina-meme-search-example)
-
-<a href="https://github.com/jina-ai/jina/"><img src="https://github.com/alexcg1/jina-app-store-example/blob/a8f64332c6a5b3ae42df07d4bd615ff1b7ece4d9/frontend/powered_by_jina.png?raw=true" width=256></a>
-""",
-    unsafe_allow_html=True,
-)
+            cell.image("http:" + match["tags"]["image_url"])
--- config.py
+++ config.py
@@ -4,5 +4,7 @@ text_endpoint = "http://3.138.155.170:45679/search"
 # Image search
 image_endpoint = "http://0.0.0.0:45680/search"
 images_path = "../data/images/"
+image_size = 128 # How big to render images
 
 # General
+top_k = 10
