eb93e9a62f10f8e6f1bd86424a90b52189298cf7
Alex C-G <alexcg@outlook.com>
2021-08-25 16:49:21 +0200

    Revert "fix: reduce to one sample for mobile"
    This reverts commit 821fd2ac293753ac71825825b2b1088b8cc749fd.

--- image_search.py
+++ image_search.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import random
 import os
 from config import image_endpoint, top_k
 from helper import search_by_file, search_by_text, UI, create_temp_file
@@ -33,8 +32,7 @@ st.title("Search memes by image")
     # query = st.text_input("Search phrase")
     # search_fn = search_by_text
 # else:
-header_cell, upload_cell, preview_cell =  st.columns([4, 7, 1])
-header_cell.header("Search by file...")
+upload_cell, preview_cell =  st.columns([12, 1])
 query = upload_cell.file_uploader("Upload file")
 search_fn = search_by_file
 if query:
@@ -48,31 +46,18 @@ if query:
 
 # Sample image list
 else:
-    sample_files = []
-    for filename in os.listdir("./samples"):
-        sample_files.append(filename)
-    sample_image = random.choice(sample_files)
-    meme_name = sample_image.split(".")[0]
+    sample_box = st.expander(label="Search from a sample", expanded=True)
+    with sample_box:
+        sample_files = []
+        for filename in os.listdir("./samples"):
+            sample_files.append(filename)
+        sample_cells = st.columns(len(sample_files))
 
-    header_cell, sample_preview = st.columns[4, 8]
-    header_cell.header("...search from an existing meme")
-    sample_preview.image(f"samples/{sample_image}", width=128)
-    if sample_preview.button(f"{meme_name}"):
-        matches = search_by_file(endpoint, top_k, f"samples/{sample_image}")
-    # sample_box = st.expander(label="Search from a sample", expanded=True)
-
-    # sample_cells = st.columns(len(sample_files))
-
-    # with sample_box:
-        # for filename in os.listdir("./samples"):
-            # sample_files.append(filename)
-        # sample_cells = st.columns(len(sample_files))
-
-        # for cell, filename in zip(sample_cells, sample_files):
-            # meme_name = filename.split(".")[0]
-            # cell.image(f"samples/{filename}", width=128)
-            # if cell.button(f"{meme_name}"):
-                # matches = search_by_file(endpoint, top_k, f"samples/{filename}")
+        for cell, filename in zip(sample_cells, sample_files):
+            meme_name = filename.split(".")[0]
+            cell.image(f"samples/{filename}", width=128)
+            if cell.button(f"{meme_name}"):
+                matches = search_by_file(endpoint, top_k, f"samples/{filename}")
 
         # if media_type == "Image" or media_type == "Video":
 # Set up grid
