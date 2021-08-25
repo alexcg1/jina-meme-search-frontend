33e3c6a22449476b283da1c8aeb9870401576779
Alex C-G <alexcg@outlook.com>
2021-08-25 14:56:16 +0200

    feat: sample grid

--- image_search.py
+++ image_search.py
@@ -1,9 +1,10 @@
 import streamlit as st
+import os
 from config import image_endpoint, top_k
 from helper import search_by_file, search_by_text, UI, create_temp_file
 
 endpoint = image_endpoint
-
+matches = []
 
 st.markdown(
     body=UI.css,
@@ -31,25 +32,40 @@ st.title("Search memes by image")
     # query = st.text_input("Search phrase")
     # search_fn = search_by_text
 # else:
+st.subheader("Upload an image...")
 upload_cell, preview_cell =  st.columns([12, 1])
 query = upload_cell.file_uploader("Upload file")
 search_fn = search_by_file
 if query:
     uploaded_image = create_temp_file(query)
     preview_cell.image(uploaded_image)
+    if st.button(label="Search"):
+        if not query:
+            st.markdown("Please enter a query")
+        else:
+            matches = search_by_file(endpoint, top_k, "/tmp/query.png")
 
-if st.button(label="Search"):
-    if not query:
-        st.markdown("Please enter a query")
-    else:
-        matches = search_by_file(endpoint, top_k, "/tmp/query.png")
+# Sample image list
+else:
+    st.subheader("...or search similar memes")
+    sample_cells = st.columns(4)
+    sample_files = []
+    for filename in os.listdir("./samples"):
+        sample_files.append(filename)
 
+    for cell, filename in zip(sample_cells, sample_files):
+        meme_name = filename.split(".")[0]
+        cell.image(f"samples/{filename}", width=128)
+        if cell.button(f"{meme_name}"):
+            matches = search_by_file(endpoint, top_k, f"samples/{filename}")
+
+print(matches)
         # if media_type == "Image" or media_type == "Video":
-    # Set up grid
-        cell1, cell2, cell3 = st.columns(3)
-        cell4, cell5, cell6 = st.columns(3)
-        cell7, cell8, cell9 = st.columns(3)
-        all_cells = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9]
+# Set up grid
+cell1, cell2, cell3 = st.columns(3)
+cell4, cell5, cell6 = st.columns(3)
+cell7, cell8, cell9 = st.columns(3)
+all_cells = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9]
 
-        for cell, match in zip(all_cells, matches):
-            cell.image(match["tags"]["uri_absolute"], use_column_width="auto")
+for cell, match in zip(all_cells, matches):
+    cell.image(match["tags"]["uri_absolute"], use_column_width="auto")
