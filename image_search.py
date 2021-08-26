51766ebdf168a445eda5f3cc30ccca3923e81411
Alex C-G <alexcg@outlook.com>
2021-08-26 16:35:27 +0200

    feat: integrated text search

--- image_search.py
+++ image_search.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import random
 import os
-from config import image_endpoint, top_k
+from config import image_endpoint, text_endpoint, top_k
 from helper import search_by_file, search_by_text, UI, create_temp_file
 
 endpoint = image_endpoint
@@ -12,67 +12,82 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+media_type = st.sidebar.radio(
+    label="Search using", options=["Text", "Image"], index=1
+)
+
 st.markdown(UI.repo_banner, unsafe_allow_html=True)
 # Sidebar
 st.sidebar.markdown(UI.text_block, unsafe_allow_html=True)
 st.sidebar.markdown(UI.image_repo_block, unsafe_allow_html=True)
 
-# media_type = st.sidebar.radio(
-    # label="Media type", options=["Text", "Image", "Audio", "Video"], index=1
-# )
-
-settings = st.sidebar.expander(label="Settings", expanded=True)
+settings = st.sidebar.expander(label="Settings")
 with settings:
-    endpoint = st.text_input(label="Endpoint", value=endpoint)
+    image_endpoint = st.text_input(label="Image endpoint", value=image_endpoint, key="image_endpoint")
+    text_endpoint = st.text_input(label="Text endpoint", value=text_endpoint, key="text_endpoint")
     top_k = st.number_input(label="Top K", value=top_k, step=1)
 
 # Main area
 # st.title(f"Search {media_type.lower()} by {media_type.lower()}")
-st.title("Search memes by image")
+st.title(f"Search memes by {media_type.lower()}")
 
 # if media_type == "Text":
-    # query = st.text_input("Search phrase")
-    # search_fn = search_by_text
 # else:
-st.header("Search from your own image...")
-upload_cell, preview_cell =  st.columns([12, 1])
-query = upload_cell.file_uploader("Upload file")
-search_fn = search_by_file
-if query:
-    uploaded_image = create_temp_file(query)
-    preview_cell.image(uploaded_image)
-    if st.button(label="Search"):
-        if not query:
-            st.markdown("Please enter a query")
-        else:
-            matches = search_by_file(endpoint, top_k, "/tmp/query.png")
+if media_type == "Image":
+    st.header("Search from your own image...")
+    upload_cell, preview_cell =  st.columns([12, 1])
+    query = upload_cell.file_uploader("Upload file")
+    if query:
+        uploaded_image = create_temp_file(query)
+        preview_cell.image(uploaded_image)
+        if st.button(label="Search"):
+            if not query:
+                st.markdown("Please enter a query")
+            else:
+                matches = search_by_file(image_endpoint, top_k, "/tmp/query.png")
 
-# Sample image list
+    # Sample image list
+    else:
+        st.header("...or search from an existing meme")
+        sample_files = []
+        for filename in os.listdir("./samples"):
+            sample_files.append(filename)
+        # random.shuffle(sample_files)
+        sample_cells = st.columns(len(sample_files))
+
+        for cell, filename in zip(sample_cells, sample_files):
+            meme_name = filename.split(".")[0]
+            cell.image(f"samples/{filename}", width=128)
+            if cell.button(f"{meme_name}", key=meme_name):
+                matches = search_by_file(image_endpoint, top_k, f"samples/{filename}")
+
+        # sample_meme = sample_files[0]
+        # meme_name = sample_meme.split(".")[0]
+        # st.image(f"samples/{sample_meme}", width=256)
+        # if st.button(f"{meme_name}", key=meme_name):
+            # matches = search_by_file(endpoint, top_k, f"samples/{sample_meme}")
+
+    # Set up grid
 else:
-    st.header("...or search from an existing meme")
-    sample_files = []
-    for filename in os.listdir("./samples"):
-        sample_files.append(filename)
-    # random.shuffle(sample_files)
-    sample_cells = st.columns(len(sample_files))
+    st.subheader("Search with your own text...")
+    query = st.text_input("Search phrase", key="text_search_box")
+    search_fn = search_by_text
+    if st.button("Search", key="text_search"):
+        matches = search_by_text(query, text_endpoint, top_k)
+    st.subheader("...or search from a sample")
+    sample_texts = ["squidward school", "so you're telling me willy wonka", "seagull kitkat"]
+    for text in sample_texts:
+        if st.button(text):
+            matches = search_by_text(text, text_endpoint, top_k)
 
-    for cell, filename in zip(sample_cells, sample_files):
-        meme_name = filename.split(".")[0]
-        cell.image(f"samples/{filename}", width=128)
-        if cell.button(f"{meme_name}", key=meme_name):
-            matches = search_by_file(endpoint, top_k, f"samples/{filename}")
-
-    # sample_meme = sample_files[0]
-    # meme_name = sample_meme.split(".")[0]
-    # st.image(f"samples/{sample_meme}", width=256)
-    # if st.button(f"{meme_name}", key=meme_name):
-        # matches = search_by_file(endpoint, top_k, f"samples/{sample_meme}")
-
-# Set up grid
+print(matches)
 cell1, cell2, cell3 = st.columns(3)
 cell4, cell5, cell6 = st.columns(3)
 cell7, cell8, cell9 = st.columns(3)
 all_cells = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9]
 
 for cell, match in zip(all_cells, matches):
-    cell.image(match["tags"]["uri_absolute"], use_column_width="auto")
+    if media_type == "Text":
+        cell.image("http:" + match["tags"]["image_url"])
+    else:
+        cell.image(match["tags"]["uri_absolute"], use_column_width="auto")
