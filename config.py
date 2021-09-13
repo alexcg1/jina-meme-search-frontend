48b14aef9e803b82f001671ceadc46ef0822b511
Alex C-G <alexcg@outlook.com>
2021-09-13 17:16:26 +0200

    fix: update config

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+server_url = "https://examples.jina.ai"
+
 # Text search
 text_endpoint = "http://examples.jina.ai:45678/search"
 text_endpoint = "http://0.0.0.0:45679/search"
@@ -5,8 +7,10 @@ text_endpoint = "http://0.0.0.0:45679/search"
 # Image search
 image_endpoint = "http://examples.jina.ai:12345/search"
 image_endpoint = "http://0.0.0.0:12345/search"
+image_endpoint = "http://0.0.0.0:54321/search"
 images_path = "../data/images/"
 image_size = 128 # How big to render images
 
 # General
 top_k = 10
+score_filter = 1
